44ce5e9fe58853b4f0af363c20c4766bdfa73fa7
Aldebarancloud <45065026+Aldebarancloud@users.noreply.github.com>
2021-04-07 17:30:06 +0200

    Delete ou.ps1

--- Lab1/PrerequisiteID/AzureAdConnect/ou.ps1
+++
@@ -1,8 +0,0 @@
-param( 
-[parameter(Mandatory=$true)][string]$ouUsers,
-[parameter(Mandatory=$true)][string]$ouHosts
-)
-
-New-ADOrganizationalUnit -Name $ouUsers
-New-ADOrganizationalUnit -Name $ouHosts
-
