f6a9f1b06cf59bfc8eb863b41c64322122054e86
Aldebarancloud <45065026+Aldebarancloud@users.noreply.github.com>
2021-04-13 22:05:21 +0200

    Create ChangeRegionalFormat.ps1

---
+++ Lab5GoldenImageConfiguration/GoldenImageConfiguration/Regionalformat/ChangeRegionalFormat.ps1
@@ -0,0 +1,22 @@
+$CultureInfo = "fr-FR"
+Set-Culture -CultureInfo $CultureInfo
+#Remplace the value after $CultureInfo by the value you want
+
+# A small list of the common below the rest is on this link https://docs.microsoft.com/en-us/openspecs/windows_protocols/ms-lcid/a9eac961-e77d-41a6-90a5-ce1a8b0cdb9c
+#de-DE	German-Germany
+#de-CH German-Switzerland
+#fr-CH	French-Switzerland
+#en-US English-United States
+#en-GB English-United Kingdom 
+#it-IT	Italian-Italy
+#en-IE	English-Ireland
+#75	Czech Republic
+#nn-NO	Norwegian (Nynorsk)-Norway
+#fr-FR	French-France
+#nl-NL	Dutch-Netherlands
+#pt-PT Portuguese-Portugal
+#es-ES	Spanish-Spain
+
+
+
+
