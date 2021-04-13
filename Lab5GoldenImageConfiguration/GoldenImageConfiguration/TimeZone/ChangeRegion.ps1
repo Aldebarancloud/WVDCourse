48f2902e99ddfb21064aa239d897d6c6b217b0e4
Aldebarancloud <45065026+Aldebarancloud@users.noreply.github.com>
2021-04-13 22:05:27 +0200

    Create ChangeRegion.ps1

---
+++ Lab5GoldenImageConfiguration/GoldenImageConfiguration/TimeZone/ChangeRegion.ps1
@@ -0,0 +1,31 @@
+$GeoID = "84"
+Set-WinHomeLocation -GeoId $GeoID
+#Remplace the value after $GeoId by the value you want
+
+# A small list of the common below the rest is on this link https://docs.microsoft.com/en-us/windows/win32/intl/table-of-geographical-locations
+#12	Australia
+#14	Austria
+#21	Belgium
+#35	Bulgaria
+#39	Canada
+#61	Denmark
+#68	Ireland
+#75	Czech Republic
+#77	Finland
+#84	France
+#94	Germany
+#98	Greece
+#108	Croatia
+#109	Hungary
+#118	Italy
+#176	Netherlands
+#177	Norway
+#191	Poland
+#193	Portugal
+#212	Slovenia
+#217	Spain
+#221	Sweden
+#223	Switzerland
+#241	Ukraine
+#242	United Kingdom
+#244	United States
