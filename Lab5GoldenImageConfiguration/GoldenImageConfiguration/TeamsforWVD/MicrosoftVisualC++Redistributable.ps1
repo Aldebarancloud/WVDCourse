11db23f16ea6522161aa184757323790cffb4f50
Aldebarancloud <45065026+Aldebarancloud@users.noreply.github.com>
2021-04-14 18:59:16 +0200

    Create MicrosoftVisualC++Redistributable.ps1

---
+++ Lab5GoldenImageConfiguration/GoldenImageConfiguration/TeamsforWVD/MicrosoftVisualC++Redistributable.ps1
@@ -0,0 +1,10 @@
+#VisualC++ Source x64
+$VisualCSource = "https://aka.ms/vs/16/release/vc_redist.x64.exe"
+$location = "D:\"
+$pathvcc = "D:\vc_redist.x64.exe"
+
+#Download VisualC++ x64
+Start-BitsTransfer -Source $VisualCSource -Destination $location
+Start-Process -FilePath "$pathvcc" /quiet
+
+#the VM Will restart
