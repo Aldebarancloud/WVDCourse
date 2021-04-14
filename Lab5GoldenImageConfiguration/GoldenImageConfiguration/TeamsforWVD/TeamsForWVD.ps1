0c88e688db82f4c3954a8717903fcc359d472518
Aldebarancloud <45065026+Aldebarancloud@users.noreply.github.com>
2021-04-14 18:59:11 +0200

    Create TeamsForWVD.ps1

---
+++ Lab5GoldenImageConfiguration/GoldenImageConfiguration/TeamsforWVD/TeamsForWVD.ps1
@@ -0,0 +1,21 @@
+#Variables Teams
+$TeamsSource = "https://statics.teams.cdn.office.net/production-windows-x64/1.3.00.21759/Teams_windows_x64.msi"
+$locationteamsdownload = "C:\"
+$locationteams = "C:\Teams_windows_x64.msi"
+
+#Regedit Teams for WVD
+reg add "HKLM\SOFTWARE\Microsoft\Teams" /v IsWVDEnvironment /t REG_DWORD /d 1 /f
+
+#Download Teams Source x64
+Start-BitsTransfer -Source $TeamsSource -Destination $locationteamsdownload
+
+#Run this following command to install Teams
+msiexec /i $locationteams /l*v teams_install.log ALLUSERS=1 ALLUSER=1
+
+"https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true"
+
+#Fallback mode in Teams
+#audio only
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Teams\DisableFallback" /t REG_DWORD /d 1 /f
+#Disable
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Teams\DisableFallback" /t REG_DWORD /d 0 /f
