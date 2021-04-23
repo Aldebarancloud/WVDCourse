42f57db68c5e3aab87a73e0507762af339a34a90
Aldebarancloud <45065026+Aldebarancloud@users.noreply.github.com>
2021-04-23 09:27:26 +0200

    Update ManagementTools.ps1

--- Lab1AzureDomainService/PrerequisiteID/CustomScriptExtention/ManagementTools.ps1
+++ Lab1AzureDomainService/PrerequisiteID/CustomScriptExtention/ManagementTools.ps1
@@ -1,5 +1,8 @@
-$LDAP= "RSAT"
+$LDAP= "RSAT-ADDS"
 $GroupPolicy= "GPMC"
+
+Add-WindowsFeature -Name $LDAP
+Add-WindowsFeature -Name $GroupPolicy
 
 Install-WindowsFeature –Name $LDAP
 Install-WindowsFeature –Name $GroupPolicy
